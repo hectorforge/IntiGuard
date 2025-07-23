37305a043838769315203bf14ba06141c99ab2f5
hectorforge <hector.hernandez.dev@gmail.com>
2025-07-23 13:13:43 -0500

    Estructura inicial del proyecto ASP.NET Core sin archivos temporales

---
+++ IntiGuard/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IntiGuard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
