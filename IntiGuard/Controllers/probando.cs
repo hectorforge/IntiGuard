9e11721cfbca04c2e47dd7cd183db413030323cb
Gabriel <gabrielfloresgaragundo.1999@gmail.com>
2025-07-24 11:05:34 -0500

    probando cambios

---
+++ IntiGuard/Controllers/probando.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IntiGuard.Controllers
+{
+    public class probando : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
