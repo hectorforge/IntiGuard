13b9e4f6ddb35b6e1b826e78d90b619654624c2b
Gabriel <gabrielfloresgaragundo.1999@gmail.com>
2025-07-27 19:41:39 -0500

    Eliminar carrito_compra y agregar un paquete ViewModel con su carrito

---
+++ IntiGuard/ViewModel/Carrito.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IntiGuard.ViewModel
+{
+    public class Carrito
+    {
+        public int id_carrito { get; set; }
+
+        public int id_usuario { get; set; }
+
+        public int id_producto { get; set; }
+
+        public int cantidad { get; set; }
+    }
+}
